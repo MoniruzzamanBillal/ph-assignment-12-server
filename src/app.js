1db80ee26503dc7f334df06ec5cdd85b63a1ad0b
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2023-11-27 10:24:26 +0600

    jwt added , token vewrify added , user role find api added

--- src/app.js
+++ src/app.js
@@ -45,34 +45,16 @@ async function run() {
 
     //*-------------------------------------------------------------------------------
 
-    // ! making  middlewire
-    const verifyToken = (req, res, next) => {
-      // console.log(req.headers.cookie);
-      const token = req?.headers?.cookie;
-      if (!token) {
-        return res.status(401).send({ message: "forbidden access" });
-      }
-
-      jwt.verify(token, process.env.Access_Token_SECRET, (error, decoded) => {
-        if (error) {
-          return res.status(401).status({ message: "forbidden access" });
-        }
-
-        req.decoded = decoded;
-
-        next();
-      });
-    };
-    // ! making  middlewire ends
     //*-------------------------------------------------------------------------------
 
     // ! creating token
 
-    app.post("/jwt", async (req, res) => {
+    app.post("/jwt2", async (req, res) => {
       try {
         const data = req.body;
+        console.log("data in jwt  = ", data);
 
-        const token = jwt.sign(data, process.env.Access_Token_SECRET, {
+        const token = jwt.sign(data, process.env.TOKEN_SECRET, {
           expiresIn: "2h",
         });
 
@@ -87,8 +69,71 @@ async function run() {
       }
     });
 
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("user in jwt path = ", user);
+      const token = jwt.sign(user, process.env.TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      // console.log("token in jwt = ", token);
+
+      res.send({ token });
+    });
+
     // ! creating token  ends
     //*-------------------------------------------------------------------------------
+
+    // ! making  middlewire
+
+    const verifyToken = (req, res, next) => {
+      // console.log("inside middlewire = ", req.headers.authorization);
+
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access " });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+
+      jwt.verify(token, process.env.TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: "forbidden access " });
+        }
+
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    const verifyToken2 = (req, res, next) => {
+      const token = req?.headers?.cookie;
+
+      console.log("--------------------------");
+      console.log("token in middlewire ", token);
+      console.log("--------------------------");
+
+      jwt.verify(token, process.env.TOKEN_SECRET, (error, decoded) => {
+        console.log("--------------------------");
+        console.log("inside verify ");
+        console.log("--------------------------");
+
+        console.log("--------------------------");
+        console.log("decoded = ", decoded);
+        console.log("error in verify = ", error);
+        console.log("--------------------------");
+        if (error) {
+          return res.status(401).status({ message: "forbidden access" });
+        }
+
+        console.log("--------------------------");
+        console.log("after error ");
+        console.log("--------------------------");
+
+        req.decoded = decoded;
+        console.log("hit next");
+        next();
+      });
+    };
+    // ! making  middlewire ends
 
     //*-------------------------------------------------------------------------------
 
@@ -99,6 +144,26 @@ async function run() {
       const data = await usersCollection.find().toArray();
 
       res.send(data);
+    });
+
+    // user role related api
+    app.get("/user/role/:email", verifyToken, async (req, res) => {
+      const requestedEmail = req.params.email;
+
+      // console.log(requestedEmail);
+
+      if (requestedEmail !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access " });
+      }
+
+      const query = {
+        email: requestedEmail,
+      };
+
+      const user = await usersCollection.findOne(query);
+
+      console.log("user in role api = ", user.role);
+      res.send({ role: user.role });
     });
 
     // create user in database
@@ -129,7 +194,10 @@ async function run() {
     // ! parcels realted api
 
     // get all parcel data
-    app.get("/parcels", async (req, res) => {
+    app.get("/parcels", verifyToken, async (req, res) => {
+      // app.get("/parcels", async (req, res) => {
+      // console.log("verified user = ", req.decoded);
+      // console.log("data in header = ", req.headers);
       try {
         let query = {};
         if (req?.query?.email) {
@@ -139,7 +207,7 @@ async function run() {
         }
 
         // userEmail
-        console.log("query in server = ", query);
+        // console.log("query in server = ", query);
         // console.log("email in server = ", req?.query);
         const parcelResponse = await parcelsCollection.find(query).toArray();
         res.send(parcelResponse);
